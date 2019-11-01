33b0fce301ee69b5bd046dfa0a5638803effe341
michael <mikelkarije@gmail.com>
2019-11-01 16:14:00 +0300

    addition of this commit will make changes to forms html and the gitsearch components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
@@ -14,7 +14,8 @@ import { GitsearchComponent } from './gitsearch/gitsearch.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
