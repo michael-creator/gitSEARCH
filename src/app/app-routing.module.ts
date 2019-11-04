6bc996b7f8600a2881fbac42965ca25640f61d36
michael <mikelkarije@gmail.com>
2019-11-04 17:36:16 +0300

    new app routes created users and for repos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {GitsearchComponent} from './gitsearch/gitsearch.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-
-const routes: Routes = [{path:'**',component:GitsearchComponent}];
+const routes: Routes = [
+  {path:'**',component:GitsearchComponent},
+  {path:'repository',component:RepositoryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
