af0d289c19b8124e31501857e576095f57b888da
michael <mikelkarije@gmail.com>
2019-11-04 17:17:28 +0300

    this commit adds new user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,3 +1,8 @@
-export class Gitsearch {
-    public
+export class User {
+    
+    constructor(
+        public name:string, 
+        public avatar:string,
+        public created_at:string,
+        public updated_at:string){}
 }
