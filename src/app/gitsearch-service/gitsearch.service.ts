c285d1e04f5be62fba5e5756d0583a289773b731
michael <mikelkarije@gmail.com>
2019-11-04 17:02:47 +0300

    this commit will add new methods and also and links to my work app

--- src/app/gitsearch-service/gitsearch.service.ts
+++ src/app/gitsearch-service/gitsearch.service.ts
@@ -1,12 +1,47 @@
-import { Gitsearch } from '../user';
+import { User} from '../user';
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitsearchService {
-getGitsearchs(){
-  return Gitsearch
-}
-  constructor() { }
+
+  user:User;
+  
+  constructor(private http:HttpClient) 
+  { this.user= new User("Test name","Test avatar", "Test created", "Test updated"); }
+
+  getUser(username:string){
+      let url = "https://api.github.com/users/"+username+"?access_token=e3cdec744a262f5ff693def1beb0173c058257e1";
+
+      let promise = new Promise(
+        (resolve,reject)=>{
+
+          this.http.get(url).toPromise().then(
+            (results)=>{ 
+              
+              this.user=new User(
+              results["login"],
+              results["avatar_url"],
+              results["created_at"],
+              results["updated_at"]
+            );   
+
+            resolve();
+          
+          },
+            (error)=>{
+              this.user = new User("Error","Error","Error","Error");
+              reject();
+
+           }
+          )
+
+        }
+      );
+
+    return promise;
+  }
 }
